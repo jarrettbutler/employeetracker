cd495c5bdac637addca1daa81553989a79bbdf2b
jarrettbutler <jbutler98@hotmail.com>
2023-01-27 19:58:16 -0400

    Started the inquirer prompts and functions. Ran into a problem with inquirer will need to fix ASAP.

---
+++ server.js
@@ -0,0 +1,52 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Router1!',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the movies_db database.`)
+);
+
+const mainMenu = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'mainMenu',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
+        },])
+        .then((answer) => {
+            switch (answer.mainMenu) {
+                case 'View All Employees':
+                    viewAllEmployees();
+                    break;
+                case 'Add Employee':
+                    addEmployee();
+                    break;
+                case 'Update Employee Role':
+                    updateEmployeeRole();
+                    break;
+                case 'View All Roles':
+                    viewAllRoles();
+                    break;
+                case 'Add Role':
+                    addRole();
+                    break;
+                case 'View All Departments':
+                    viewAllDepartments();
+                    break;
+                case 'Add Department':
+                    addDepartment();
+                    break;
+                case 'Quit':
+                    quit();
+                    break;
+            }
+        })
+}
+
+mainMenu();
