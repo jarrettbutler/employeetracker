f5199b937469909127db49f41e3047d7c10cb9e9
jarrettbutler <jbutler98@hotmail.com>
2023-01-29 14:38:54 -0400

    Just need to figure out how to get the values from the database and insert them as options for the choices in the add/update functions

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const db = mysql.createConnection(
     console.log(`Connected to the employees_db database.`)
 );
 
+//Creating an array with all the choices in it for roles and departments 
+//These roles and departments will become a strong that is used as the choices
+//In the add functions
+
+
 //Main menu prompt whenever a function is executed it will come back here
 const mainMenu = () => {
     inquirer.prompt([
@@ -46,12 +51,13 @@ const mainMenu = () => {
                     addDepartment();
                     break;
                 case 'Quit':
-                    quit();
+                    console.log(`Have a nice day to confirm you'd like to quit the program please press ctrl + c`);
                     break;
             }
         })
 }
 
+//Allows us to view all employees and information if there is an error and error message will appear if sucessful a table will display
 const viewAllEmployees = () => {
     const employeeTable = `SELECT employees.id AS ID, employees.first_name AS 'First Name', employees.last_name AS 'Last Name', roles.title AS Title, departments.name AS Department, roles.salary AS Salary, CONCAT(manager.first_name, " ", manager.last_name) AS Manager
     FROM employees
@@ -69,6 +75,88 @@ const viewAllEmployees = () => {
     })
 };
 
+//Function add employee based on user choice if err an error message will appear if not the message will appear and the employee will be added to the DB
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'Input',
+            name: 'employeeFirstName',
+            message: 'What is the first name of the employee?',
+            validate: (firstName) => {
+                if (firstName) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid first name');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'number',
+            name: 'employeeLastName',
+            message: 'What is the last name of the employee?',
+            validate: (lastName) => {
+                if (lastName) {
+                    return true;
+                } else {
+                    console.log('Please enter a valid last name');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'list',
+            name: 'employeeRole',
+            message: 'What is the employees role?',
+            choices: role,
+        },
+        {
+            type: 'list',
+            name: 'employeeManager',
+            message: 'What is the employees manager?',
+            choices: manager,
+        }
+    ]).then((answer) => {
+        db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${answer.employeeFirstName}', ${answer.employeeLastName}, ${answer.employeeRole}, ${answer.employeeManager})`, (err, result) => {
+            if (err) {
+                console.log('There was an error adding this employee to the database', err);
+                return;
+            } else {
+                console.log(`${answer.employeeFirstName} ${answer.employeeLastName} has been added to employees as the ${answer.employeeRole} under ${answer.employeeManager}`);
+            };
+        });
+        mainMenu();
+    });
+};
+
+//Function that updates a employee job based on user input if theres an error it will show a message on success a message will appear and the DB will be updated
+const updateEmployeeRole = () => {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'updateEmployee',
+            message: 'What employee would you like to update?',
+            choices: employees
+        },
+        {
+            type: 'list',
+            name: 'updateRole',
+            message: 'What new role would you like to assign this employee?',
+            choices: roles
+        },
+    ]).then((answer) => {
+        db.query(`UPDATE employees SET role_id = '${answer.updateRole}' WHERE id = ${answer.updateEmployee}`, (err, result) => {
+            if (err) {
+                console.log('There was an error updating this employee in the database', err);
+            } else {
+                console.log(`${answer.updateEmployee} has been updated to `)
+            }
+        });
+        mainMenu();
+    });
+};
+
+//Allows us to view all roles and information if there is an error and error message will appear if sucessful a table will display
 const viewAllRoles = () => {
     const rolesTable = `SELECT roles.id AS ID, roles.title AS Title, departments.name AS Department, roles.salary AS Salary 
     FROM roles
@@ -84,12 +172,13 @@ const viewAllRoles = () => {
     })
 };
 
+//Function add roles based on user choice if err an error message will appear if not the message will appear and the role will be added to the DB
 const addRole = () => {
     inquirer.prompt([
         {
             type: 'Input',
             name: 'roleTitle',
-            message: 'What is the title of the role you would like to add??',
+            message: 'What is the title of the role you would like to add?',
             validate: (roleTitle) => {
                 if (roleTitle) {
                     return true;
@@ -116,23 +205,23 @@ const addRole = () => {
             type: 'list',
             name: 'roleDepartment',
             message: 'What department does this role belong to?',
-            choices: departments, 
-            validate: (roleDepartment) => {
-                if (roleDepartment) {
-                    return true;
-                } else {
-                    console.log('Please enter a valid department');
-                    return false;
-                }
-            },
+            choices: departments,
         }
-    ]). then((answer) => {
-        db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${answer.roleTitle}', ${answer.roleSalary}, ${answer.roleDepartment})`)
-        console.log(`${answer.roleTitle} has been added to roles witht the salaray of ${answer.roleSalary} in the ${answer.roleDepartment} department`);
+    ]).then((answer) => {
+        db.query(`INSERT INTO role (title, salary, department_id) VALUES ('${answer.roleTitle}', ${answer.roleSalary}, ${answer.roleDepartment})`, (err, result) => {
+            if (err) {
+                console.log('There was an error adding this role to the database', err);
+                return;
+            } else {
+                console.log(`${answer.roleTitle} has been added to roles with the salaray of ${answer.roleSalary} in the ${answer.roleDepartment} department`);
+            };
+        });
         mainMenu();
-    })
-}
+    });
+};
 
+
+//Allows us to view all departments if there is an error and error message will appear if sucessful a table will display
 const viewAllDepartments = () => {
     const departmentTable = `SELECT departments.id AS ID, departments.name AS name FROM departments`;
     db.query(departmentTable, (err, result) => {
@@ -146,6 +235,7 @@ const viewAllDepartments = () => {
     })
 };
 
+//Function add departments based on user choice if err an error message will appear if not the message will appear and the department will be added to the DB
 const addDepartment = () => {
     inquirer.prompt([
         {
@@ -161,11 +251,18 @@ const addDepartment = () => {
                 }
             },
         }
-    ]). then((answer) => {
-        db.query(`INSERT INTO departments (name) VALUES ('${answer.department}')`)
-        console.log(`${answer.department} has been added to departments`);
+    ]).then((answer) => {
+        db.query(`INSERT INTO departments (name) VALUES ('${answer.department}')`, (err, result) => {
+            if (err) {
+                console.log('There was an error adding this department to the database', err);
+                return;
+            } else {
+                console.log(`${answer.department} has been added to departments`);
+            };
+        });
         mainMenu();
     })
-}
+};
 
+//Starts the first main menu prompt
 mainMenu();
